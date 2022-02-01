2577a27d7e377bdbc51e07060d5919cd50af59e8
Chris <christangarife95@gmail.com>
2022-02-01 13:08:57 -0500

    styling of the contact pag/form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -20,11 +20,11 @@ export const Contact = () => {
     <div>
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" id='inputs' />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" id='inputs' />
       <label>Message</label>
-      <textarea name="message" />
+      <textarea name="message" id='inputs' />
       <input type="submit" value="Send" id='button'/>
     </form>
     </div>
